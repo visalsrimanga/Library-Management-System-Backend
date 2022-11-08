be38beb9b33da3a9976518d0ac1f1b1170c50ef5
visalsrimanga <visalsrimanga@gmail.com>
2022-11-08 17:52:47 +0530

    finish load all books and search books

--- src/main/java/lk/ijse/dep9/api/BookServlet.java
+++ src/main/java/lk/ijse/dep9/api/BookServlet.java
@@ -1,17 +1,195 @@
 package lk.ijse.dep9.api;
 
+import jakarta.annotation.Resource;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.json.bind.JsonbConfig;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import lk.ijse.dep9.api.util.HttpServlet2;
+import lk.ijse.dep9.dto.BookDTO;
 
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-@WebServlet(name = "BooksServlet", value = "/books")
+@WebServlet(name = "BooksServlet", value = "/books/*", loadOnStartup = 1)
 public class BookServlet extends HttpServlet2 {
+    @Resource(lookup = "java:/comp/env/jdbc/dep9-lms")
+    private DataSource pool;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("BooksServlet: doGet()");
+        if (request.getPathInfo() == null || request.getPathInfo().equals("/")){
+            String query = request.getParameter("q");
+            String size = request.getParameter("size");
+            String page = request.getParameter("page");
+
+            if (query != null && size != null && page != null){
+                if (size.matches("\\d+") && page.matches("\\d+")){
+                    searchPaginatedBooks(query, Integer.parseInt(size), Integer.parseInt(page), response);
+                } else {
+                    response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid page or size");
+                }
+            } else if (query != null) {
+                searchBooks(query, response);
+            } else if (page != null && size != null) {
+                if (size.matches("\\d+") && page.matches("\\d+")) {
+                    loadAllPaginatedBooks(Integer.parseInt(size), Integer.parseInt(page), response);
+                } else {
+                    response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid page or size");
+                }
+            } else {
+                loadAllBooks(response);
+            }
+        } else {
+            Matcher matcher = Pattern.compile("^/([\\d]{3}-[\\d]{1}-[\\d]{2}-[\\d]{6}-[\\d]{1})/?$")
+                    .matcher(request.getPathInfo());
+            if (matcher.matches()){
+                getBookDetails(matcher.group(1), response);
+            } else {
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,"Expented valid UUID");
+                response.sendError(HttpServletResponse.SC_NOT_IMPLEMENTED, "Expected valid ISBN");
+            }
+        }
+    }
+
+    private void loadAllBooks(HttpServletResponse response) throws IOException {
+        try (Connection connection = pool.getConnection()){
+            Statement stm = connection.createStatement();
+            ResultSet rst = stm.executeQuery("SELECT * FROM book");
+
+            ArrayList<BookDTO> books = new ArrayList<>();
+
+            while (rst.next()){
+                String isbn = rst.getString("isbn");
+                String title = rst.getString("title");
+                String author = rst.getString("author");
+                int copies = rst.getInt("copies");
+                books.add(new BookDTO(isbn, title, author, copies));
+            }
+
+            response.setContentType("application/json");
+            JsonbBuilder.create().toJson(books, response.getWriter());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "There is an issue while loading the database");
+        }
+    }
+    private void loadAllPaginatedBooks(int size, int page, HttpServletResponse response) throws IOException {
+        try(Connection connection = pool.getConnection()){
+            Statement stm1 = connection.createStatement();
+            ResultSet rst = stm1.executeQuery("SELECT COUNT(isbn) AS count FROM book");
+            rst.next();
+            int totalBooks = rst.getInt("count");
+            response.setIntHeader("X-Total-Count", totalBooks);
+
+            PreparedStatement stm2 = connection.prepareStatement
+                    ("SELECT * FROM book LIMIT ? OFFSET ?");
+            stm2.setInt(1, size);
+            stm2.setInt(2, (page -1)*size);
+            rst = stm2.executeQuery();
+
+            ArrayList<BookDTO> books = new ArrayList<>();
+
+            while(rst.next()){
+                String isbn = rst.getString("isbn");
+                String title = rst.getString("title");
+                String author = rst.getString("author");
+                int copies = rst.getInt("copies");
+                books.add(new BookDTO(isbn, title, author, copies));
+            }
+
+            response.setContentType("application/json");
+            JsonbBuilder.create().toJson(books, response.getWriter());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error while loading the data");
+        }
+    }
+
+    private void searchBooks(String query, HttpServletResponse response) throws IOException {
+        try(Connection connection = pool.getConnection()){
+            PreparedStatement stm = connection.prepareStatement
+                    ("SELECT * FROM book WHERE isbn LIKE ? OR title LIKE ? OR author LIKE ? OR copies LIKE ?");
+            query = "%" + query +"%";
+
+            stm.setString(1, query);
+            stm.setString(2, query);
+            stm.setString(3, query);
+            stm.setString(4, query);
+
+            ResultSet rst = stm.executeQuery();
+            ArrayList<BookDTO> books = new ArrayList<>();
+
+            while (rst.next()){
+                String isbn = rst.getString("isbn");
+                String title = rst.getString("title");
+                String author = rst.getString("author");
+                int copies = rst.getInt("copies");
+                books.add(new BookDTO(isbn, title, author, copies));
+            }
+
+            response.setContentType("application/json");
+            JsonbBuilder.create().toJson(books, response.getWriter());
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error while getting connection");
+        }
+    }
+    private void searchPaginatedBooks(String query, int size, int page, HttpServletResponse response) throws IOException {
+        try(Connection connection = pool.getConnection()){
+            PreparedStatement stm1 = connection.prepareStatement
+                    ("SELECT COUNT(isbn) AS count FROM book WHERE isbn LIKE ? OR title LIKE ? OR author LIKE ? OR copies LIKE ?");
+            query = "%" + query +"%";
+
+            stm1.setString(1, query);
+            stm1.setString(2, query);
+            stm1.setString(3, query);
+            stm1.setString(4, query);
+
+            ResultSet rst = stm1.executeQuery();
+            rst.next();
+            int totalBooks = rst.getInt("count");
+            response.setIntHeader("X-Total-Count", totalBooks);
+
+            PreparedStatement stm2 = connection.prepareStatement
+                    ("SELECT * FROM book WHERE isbn LIKE ? OR title LIKE ? OR author LIKE ? OR copies LIKE ? LIMIT ? OFFSET ?");
+
+            stm2.setString(1, query);
+            stm2.setString(2, query);
+            stm2.setString(3, query);
+            stm2.setString(4, query);
+            stm2.setInt(5, size);
+            stm2.setInt(6, (page - 1)*size);
+            rst = stm2.executeQuery();
+
+            ArrayList<BookDTO> books = new ArrayList<>();
+
+            while (rst.next()){
+                String isbn = rst.getString("isbn");
+                String title = rst.getString("title");
+                String author = rst.getString("author");
+                int copies = rst.getInt("copies");
+                books.add(new BookDTO(isbn, title, author, copies));
+            }
+            response.setContentType("application/json");
+            JsonbBuilder.create().toJson(books, response.getWriter());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error while loading the data");
+        }
+    }
+
+    private void getBookDetails(String isbn, HttpServletResponse response) throws IOException {
+        response.getWriter().println("get book details");
     }
 
     @Override
---
+++ src/main/java/lk/ijse/dep9/dto/BookDTO.java
@@ -0,0 +1,62 @@
+package lk.ijse.dep9.dto;
+
+import java.io.Serializable;
+
+public class BookDTO implements Serializable {
+    private String isbn;
+    private String title;
+    private String author;
+    private int copies;
+
+    public BookDTO() {
+    }
+
+    public BookDTO(String isbn, String title, String author, int copies) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.copies = copies;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void setCopies(int copies) {
+        this.copies = copies;
+    }
+
+    @Override
+    public String toString() {
+        return "BookDTO{" +
+                "isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", copies=" + copies +
+                '}';
+    }
+}
